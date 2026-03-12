9d84743240715ab8fe2e78b5b12dd0b3e6ed6392
charuK1002 <charukhandelwal610@gmail.com>
2026-03-12 18:55:58 +0530

    Initial setup for RGB LED control system

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,64 @@
 #include <Arduino.h>
+
 /**
  * @file main.cpp
  * @brief Embedded RGB LED Control (Digital + PWM)
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Charu Khandelwal
+ * @date 2026-03-11
  *
  * @details
  * Controls RGB LED using digital ON/OFF
  * and analog PWM brightness control.
+ * The program demonstrates basic LED control
+ * using Arduino digital and analog signals.
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
+// Define RED pin
+#define RED_PIN 9
 
- // TODO 2:
- // Define GREEN pin (Use 10)
+// Define GREEN pin
+#define GREEN_PIN 10
 
- // TODO 3:
- // Define BLUE pin (Use 11)
+// Define BLUE pin
+#define BLUE_PIN 11
 
 void setup() {
 
-    // TODO 4:
-    // Initialize Serial communication (9600 baud)
+    // Initialize Serial communication
+    Serial.begin(9600);
 
-    // TODO 5:
     // Configure RGB pins as OUTPUT
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
 
-    // TODO 6:
     // Print initialization message
+    Serial.println("RGB LED Control System Initialized");
 }
 
 void loop() {
 
     // -------- DIGITAL MODE --------
 
-    // TODO 7:
-    // Turn ON red (digital HIGH)
+    // Turn ON red
+    digitalWrite(RED_PIN, HIGH);
+    delay(1000);
 
-    // TODO 8:
     // Turn OFF red
+    digitalWrite(RED_PIN, LOW);
+    delay(1000);
 
     // -------- ANALOG (PWM) MODE --------
 
-    // TODO 9:
-    // Set RED brightness using analogWrite()
+    // Set RED brightness
+    analogWrite(RED_PIN, 255);
 
-    // TODO 10:
-    // Set GREEN brightness using analogWrite()
+    // Set GREEN brightness
+    analogWrite(GREEN_PIN, 128);
 
-    // TODO 11:
-    // Set BLUE brightness using analogWrite()
+    // Set BLUE brightness
+    analogWrite(BLUE_PIN, 64);
 
-    // TODO 12:
-    // Add delay for visible transition
+    // Delay for visible transition
+    delay(2000);
 }
